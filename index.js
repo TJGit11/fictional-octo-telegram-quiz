319db4a7ee0e2c266d94789f5c588c970369fa07
unknown <thomasjjohnson9311@gmail.com>
2023-04-11 22:24:44 -0500

    ?

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 let timer = 90;
 let quizDiv = document.querySelector("#quiz");
 let questionButton1 = document.querySelector("#answer1");
+let questionButton2 = document.querySelector("#answer2");
+let questionButton3 = document.querySelector("#answer3");
+let questionButton4 = document.querySelector("#answer4");
 // let timerId = setInterval(function () {
 //   timer -= 1;
 //   console.log(timer);
@@ -18,36 +21,66 @@ let questionButton1 = document.querySelector("#answer1");
 let questions = [
   {
     question: "Question 1",
-    answers: ["possible answer1", "possible answer2", "possible answer3"],
+    answers: [
+      "possible answer1",
+      "possible answer2",
+      "possible answer3",
+      "possible answer4",
+    ],
     correctAnswer: "possible answer 1 ",
   },
 
   {
     question: "Question 2",
-    answers: ["possible answer1", "possible answer2", "possible answer3"],
+    answers: [
+      "possible answer1",
+      "possible answer2",
+      "possible answer3",
+      "possible answer4",
+    ],
     correctAnswer: "possible answer 2 ",
   },
   {
     question: "Question 3",
-    answers: ["possible answer1", "possible answer2", "possible answer3"],
+    answers: [
+      "possible answer1",
+      "possible answer2",
+      "possible answer3",
+      "possible answer4",
+    ],
     correctAnswer: "possible answer 3 ",
   },
   ,
   {
     question: "Question 4",
-    answers: ["possible answer1", "possible answer2", "possible answer3"],
+    answers: [
+      "possible answer1",
+      "possible answer2",
+      "possible answer3",
+      "possible answer4",
+    ],
     correctAnswer: "possible answer 4 ",
   },
 
   {
     question: "Question 5",
-    answers: ["possible answer1", "possible answer2", "possible answer3"],
+    answers: [
+      "possible answer1",
+      "possible answer2",
+      "possible answer3",
+      "possible answer4",
+    ],
     correctAnswer: "possible answer 5 ",
   },
 
   {
     question: "Question 6",
-    answers: ["possible answer1", "possible answer2", "possible answer3"],
+    answers: [
+      "possible answer1",
+      "possible answer2",
+      "possible answer3",
+      "possible answer4",
+    ],
     correctAnswer: "possible answer 6 ",
   },
 ];
@@ -56,18 +89,18 @@ let currentQuestion = 0;
 renderQuestion();
 
 function renderQuestion() {
-  console.log(questions[currentQuestion].question);
+  // console.log(questions[currentQuestion].question);
   questionButton1.textContent = questions[currentQuestion].answers[0];
-  console.log(questions[currentQuestion].answers[1]);
-  console.log(questions[currentQuestion].answers[2]);
-  console.log("correct answer " + questions[currentQuestion].correctAnswer);
+  questionButton2.textContent = questions[currentQuestion].answers[1];
+  questionButton3.textContent = questions[currentQuestion].answers[2];
+  questionButton4.textContent = questions[currentQuestion].answers[3];
 }
 
 quizDiv.addEventListener("click", function (event) {
   if (event.target.matches("button")) {
     console.log("clicked!");
 
-    renderQuestion();
     currentQuestion++;
+    renderQuestion();
   }
 });
