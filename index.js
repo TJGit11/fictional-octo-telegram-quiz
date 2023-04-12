d6bb6c233e4106fd5487d9eee23d38835ffb843e
unknown <thomasjjohnson9311@gmail.com>
2023-04-11 23:34:30 -0500

    added store high scores and get high scores logic

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 let timer = 90;
 let quizDiv = document.querySelector("#quiz");
+let quizStart = document.querySelector("#startQuiz");
 let questionButton1 = document.querySelector("#answer1");
 let questionButton2 = document.querySelector("#answer2");
 let questionButton3 = document.querySelector("#answer3");
 let questionButton4 = document.querySelector("#answer4");
+// get scores
+let highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
 // let timerId = setInterval(function () {
 //   timer -= 1;
 //   console.log(timer);
@@ -84,6 +88,21 @@ let questions = [
     correctAnswer: "possible answer 6 ",
   },
 ];
+
+quizStart.addEventListener("click", function (event) {
+  if (event.target.matches("button")) {
+    console.log("clicked!");
+
+    currentQuestion++;
+    renderQuestion();
+  }
+});
+
+// store scores
+// localStorage.setItem(
+//   "Highscores",
+//   JSON.stringify([{ initials: "dre", score: 80 }])
+// );
 
 let currentQuestion = 0;
 renderQuestion();
